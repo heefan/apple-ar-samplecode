8352585f553c0ecd45a6a2e949c021bd5030b9d9
Li_Tian <Li_Tian@singaporeair.com.sg>
2024-07-23 09:21:54 +0800

    #BlueSkyVolume - empty project

---
+++ BlueSkyVolume/BlueSkyVolume/BlueSkyVolumeApp.swift
@@ -0,0 +1,34 @@
+//
+//  BlueSkyVolumeApp.swift
+//  BlueSkyVolume
+//
+//  Created by LI Tian on 22/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BlueSkyVolumeApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
